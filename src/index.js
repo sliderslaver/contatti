3c59f7449d53277f1c90842e78f64c5c8a000ddb
sliderslaver <83307556+sliderslaver@users.noreply.github.com>
2021-06-03 12:20:25 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Navbar } from 'rsuite';
+import {BrowserRouter as Router, Switch } from 'react-router-dom';
+
+
+
+ReactDOM.render(
+  <Router>
+    <Switch>
+    <App />
+    <Navbar/>
+    </Switch>
+    </Router>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
