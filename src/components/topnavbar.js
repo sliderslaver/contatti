8b47c44ca2f2b35e0f289b0d9cea4bfc1bf92d5f
sliderslaver <83307556+sliderslaver@users.noreply.github.com>
2021-06-03 12:41:42 +0200

    Update topnavbar.js

--- src/components/topnavbar.js
+++ src/components/topnavbar.js
@@ -24,7 +24,7 @@ function Topnavbar() {
           <NavDropdown.Item id='font' href="https://www.booking.com/hotel/it/relax-amp-sleep.it.html?label=gen173nr-1DCAEoggI46AdIM1gEaHGIAQGYARS4ARjIAQzYAQPoAQGIAgGoAgS4Aszq3IUGwAIB0gIkYzEwYTc3ZjktYzk5NS00NmQ1LTk0NDgtOTc5MjEzNzQ0MDFk2AIE4AIB&sid=7a3d698f0aa6e9c582f2bf87c8f79bfb&all_sr_blocks=391486602_122461509_2_0_0&checkin=2021-06-10&checkout=2021-06-11&dest_id=-114787&dest_type=city&dist=0&group_adults=2&group_children=0&hapos=1&highlighted_blocks=391486602_122461509_2_0_0&hpos=1&no_rooms=1&room1=A%2CA&sb_price_type=total&sr_order=popularity&sr_pri_blocks=391486602_122461509_2_0_0__3200&srepoch=1622619497&srpvid=b4ed35b4119b00c9&type=total&ucfs=1&activeTab=photosGallery">triple</NavDropdown.Item>
           <NavDropdown.Item id='font' href="https://www.booking.com/hotel/it/relax-amp-sleep.it.html?label=gen173nr-1DCAEoggI46AdIM1gEaHGIAQGYARS4ARjIAQzYAQPoAQGIAgGoAgS4Aszq3IUGwAIB0gIkYzEwYTc3ZjktYzk5NS00NmQ1LTk0NDgtOTc5MjEzNzQ0MDFk2AIE4AIB&sid=7a3d698f0aa6e9c582f2bf87c8f79bfb&all_sr_blocks=391486602_122461509_2_0_0&checkin=2021-06-10&checkout=2021-06-11&dest_id=-114787&dest_type=city&dist=0&group_adults=2&group_children=0&hapos=1&highlighted_blocks=391486602_122461509_2_0_0&hpos=1&no_rooms=1&room1=A%2CA&sb_price_type=total&sr_order=popularity&sr_pri_blocks=391486602_122461509_2_0_0__3200&srepoch=1622619497&srpvid=b4ed35b4119b00c9&type=total&ucfs=1&activeTab=photosGallery">quadruple</NavDropdown.Item>
         </NavDropdown>
-        <Nav.Link id='font'  href="/fas">Chi siamo</Nav.Link>
+        <Nav.Link id='font'  href="https://youthful-keller-9c1458.netlify.app">Chi siamo</Nav.Link>
         <Nav.Link id='font' href="/">Contattaci</Nav.Link>
       </Nav>
     </Navbar.Collapse>
